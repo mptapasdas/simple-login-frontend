fe98a0ea51f9d5af5a8975228895f529ed1b7fef
mptapasdas <tapas.das.cer19@itbhu.ac.in>
2022-03-18 11:44:19 +0530

    minimal changes in dashboard styles

--- src/layouts/dash-board/DashBoard.jsx
+++ src/layouts/dash-board/DashBoard.jsx
@@ -8,8 +8,9 @@ const DashBoard = () => {
     const { user } = useAuthContext();
     return (
         <div className='justify-content-center mt-5 bg-secondary dashboard'>
-            <h1 className='col-12'> name : {user.name}</h1>
-            <h2 className='col-12'> email : {user.email}</h2>
+            <h2 className='col-12 bg-dark dashboard-title'>Dashboard</h2>
+            <h3 className='col-12'> name : {user.name}</h3>
+            <h4 className='col-12'> email : {user.email}</h4>
         </div>
     );
 };
